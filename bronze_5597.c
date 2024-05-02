c0927a3afbbd0cffdcbba18bcdca2bc9b12a305a
rrecital <164469950+rrecital@users.noreply.github.com>
2024-05-02 16:32:39 +0900

    bronze 5597 수정

--- bronze_5597.c
+++ bronze_5597.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #pragma warning(disable:4996)
-int main() {
-	int number[30], i, a;
-	for (;;) {
 
+int main() {
+	int stu[31] = { 0 }, num;
+	for (int i = 0;i < 28;i++) {
+		scanf("%d", &num);
+		stu[num] = num;
 	}
-	for (i = 0; i < 28; i++) {
-		scanf("%d", &a);
-		
+	for (int i = 1; i <= 30; i++) {
+		if (stu[i] == 0)printf("%d\n", i);
 	}
+	return 0;
 }
